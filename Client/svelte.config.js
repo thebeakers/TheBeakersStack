4100a5310d3febe1bf07f0470a3506394dabeef2
PrudhviBoggavarapu <codingpabs@gmail.com>
2025-01-13 01:28:58 -0600

    maybe fixed for pages

--- Client/svelte.config.js
+++ Client/svelte.config.js
@@ -1,7 +1,6 @@
 import { mdsvex } from 'mdsvex';
-// import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,11 +9,11 @@ const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	},
+        adapter: adapter(),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+        }
+    },
 
 	extensions: ['.svelte', '.svx']
 };
